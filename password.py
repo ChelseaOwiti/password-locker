0121d5d1ac78a67b7e201dec6a6335300464dd6e
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-14 14:20:49 -0400

    user class created

---
+++ password.py
@@ -0,0 +1,16 @@
+class User:
+  """
+  class containing user details
+  """
+  user_list = []
+  def __init__(self, username, password):
+    """
+    creates unique details for each instance
+    """
+    self.username = username
+    self.password = password
+    
+    
+    
+    
+
