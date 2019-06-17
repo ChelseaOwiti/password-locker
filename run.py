8b7bca858b54e3f023d3aaa60711830b5133e680
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-17 00:46:37 -0400

    functions to create and save user details added to run.py

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
+#1/usr/bin/env python3.6
+
 from password import User
 from password import Credentilas
 from random import randint #enables use of randit method to import random string
+
+def new_user(username, password):
+  """
+  creates a new user
+  """
+  new_user = User(username, password)
+  return new_user(password)
+def save_user(user):
+  """
+  save created user
+  """
+  user.save_user()
+
+  
+  
+
+  
