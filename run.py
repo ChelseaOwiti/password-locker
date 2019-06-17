edff774936b45f698a8292dfa6bd5943e98f7afd
Chelsea Owiti <chelseaowiti@gmail.com>
2019-06-17 02:15:49 -0400

    README file done

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 
 from password import User
 from password import Credentilas
-from random import randint #enables use of randit method to import random string
+
 
 def new_user(username, passwords):
   """
